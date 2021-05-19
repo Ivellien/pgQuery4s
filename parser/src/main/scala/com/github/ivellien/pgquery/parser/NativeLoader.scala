1a32587803c325bc80d53df99cb02b91ff222222
Vojtech Letal <vojtech.letal@blindspot.ai>
2021-05-19 21:08:30 +0200

    nejak me to trochu zlobilo na mem systemu, tak jsem to fixnul

--- parser/src/main/scala/com/github/ivellien/pgquery/parser/NativeLoader.scala
+++ parser/src/main/scala/com/github/ivellien/pgquery/parser/NativeLoader.scala
@@ -6,17 +6,17 @@ import org.apache.commons.io.IOUtils
 import java.io._
 import java.net.URL
 import java.nio.file.{Files, Paths}
-import scala.util.{Failure, Success, Try, Using}
+import scala.util.{Try, Using}
 
 object NativeLoader extends LazyLogging {
   def fromResources(libName: String): Unit = {
-    extractLibrary(libName) match {
-      case Success(libraryPath) => System.load(libraryPath.getAbsolutePath)
-      case Failure(exception) =>
-        throw new RuntimeException(
-          s"Could not find library $libName in resources.",
-          exception
-        )
+    extractLibrary(libName).flatMap { libraryPath =>
+      Try(System.load(libraryPath.getAbsolutePath))
+    }.recover {
+      case e: UnsatisfiedLinkError if e.toString.contains("already loaded") =>
+      // ignore : library has been already loaded
+      case o =>
+        throw new RuntimeException(s"Could not find library $libName in resources.", o)
     }
   }
 
@@ -51,7 +51,7 @@ object NativeLoader extends LazyLogging {
 
   private def platformSuffix: String =
     System.getProperty("os.name").toLowerCase match {
-      case mac if mac.contains("mac")   => "dylib"
+      case mac if mac.contains("mac") => "dylib"
       case tux if tux.contains("linux") => "so"
       case other =>
         throw new RuntimeException(s"Unknown operating system $other")
