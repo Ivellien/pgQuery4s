7fa080b7abd429f089814832401cb25b9b12ed6b
Petr Hron <petr.hron@blindspot.ai>
2021-05-22 18:53:56 +0200

    Updated Liftable implementation to use Quasiquotes

--- macros/liftable/src/main/scala/com/github/ivellien/pgquery/liftable/LiftableCaseClass.scala
+++ macros/liftable/src/main/scala/com/github/ivellien/pgquery/liftable/LiftableCaseClass.scala
@@ -22,11 +22,6 @@ object LiftableCaseClass {
     // hack: adaptation of the original code - expects to be called from a macro which contains
     // a context `c` - which is kind of a convention at this point
 
-    val prefix = Select(
-      Select(c.prefix.tree, TermName("c")),
-      TermName("universe")
-    )
-
     val T = weakTypeOf[T]
     val symbol = T.typeSymbol
 
@@ -37,123 +32,37 @@ object LiftableCaseClass {
     if (!symbol.isStatic)
       c.abort(c.enclosingPosition, s"$symbol is not static")
     def fields(tpe: Type) =
-      tpe.declarations
+      tpe.decls
         .collectFirst {
           case m: MethodSymbol if m.isPrimaryConstructor ⇒ m
         }
         .get
-        .paramss
+        .paramLists
         .head
         .map { field ⇒
           val name = field.name
-          val typeSign = tpe.declaration(name).typeSignature
-          name → typeSign
+          val typeSign = tpe.decl(name).typeSignature
+          name -> typeSign
         }
-    /* Tree produced by the following quasiquote:
-     * q"reify(${c.mirror.staticModule(symbol.fullName)}).tree"
-     */
-    val constructor = Select(
-      Apply(
-        Select(prefix, TermName("reify")),
-        List(Ident(c.mirror.staticModule(symbol.fullName)))
-      ),
-      TermName("tree")
-    )
-    val value = "value"
+
+    val constructor =
+      q"reify(${c.mirror.staticModule(symbol.fullName)}).tree"
+
     val arguments = fields(T).map {
       case (name, typeSign) ⇒
-        /* Tree produced by the following quasiquote:
-         * q"implicitly[Liftable[$typeSign]].apply(value.$name)"
-         * Another way to do this would be:
-         * q"""
-         *   val v : $typeSign = value.$name
-         *   q"$$v"
-         * """
-         *
-         * This tree is not hygienic: TermName(value) will bind to value from
-         * enclosing context, which is the tree being returned from the macro.
-         * To ensure that its name does not change in one place, but stays
-         * unchanged in another, it is placed in a dedicated val.
-         */
-        Apply(
-          Select(
-            TypeApply(
-              Select(Ident(definitions.PredefModule), TermName("implicitly")),
-              List(
-                AppliedTypeTree(
-                  Select(prefix, TypeName("Liftable")),
-                  List(TypeTree(typeSign))
-                )
-              )
-            ),
-            TermName("apply")
-          ),
-          List(Select(Ident(TermName(value)), name))
-        )
+        q"implicitly[Liftable[$typeSign]].apply(value.${name.toTermName})"
     }
-    /* Tree produced by the following quasiquote:
-     * q"Apply($constructor, List(..$arguments))"
-     */
-    val reflect = Apply(
-      Select(prefix, TermName("Apply")),
-      List(constructor, Apply(Ident(definitions.ListModule), arguments))
-    )
-    val implicitName = TermName(symbol.name.encoded ++ "Liftable")
-    /* Tree produced by the following quasiquote:
-     * q"""
-     *   implicit object $implicitName extends Liftable[$T] {
-     *     def apply(value: $T): Tree = $reflect
-     *   }
-     *   $implicitName
-     * """
-     */
-    import Flag._
 
-    Block(
-      List(
-        ModuleDef(
-          Modifiers(IMPLICIT),
-          implicitName,
-          Template(
-            List(
-              AppliedTypeTree(
-                Select(prefix, TypeName("Liftable")),
-                List(TypeTree(T))
-              )
-            ),
-            noSelfType,
-            List(
-              DefDef(
-                Modifiers(),
-                nme.CONSTRUCTOR,
-                List(),
-                List(List()),
-                TypeTree(),
-                Block(List(pendingSuperCall), Literal(Constant(())))
-              ),
-              DefDef(
-                Modifiers(),
-                TermName("apply"),
-                List(),
-                List(
-                  List(
-                    ValDef(
-                      Modifiers(PARAM),
-                      TermName(value),
-                      TypeTree(T),
-                      EmptyTree
-                    )
-                  )
-                ),
-                Select(prefix, TypeName("Tree")),
-                reflect
-              )
-            )
-          )
-        )
-      ),
-      Ident(implicitName)
-    )
+    val reflect = q"Apply($constructor, $arguments)"
+
+    val implicitName = TermName(symbol.name.encodedName.toString ++ "Liftable")
+
+    q"""
+       implicit object $implicitName extends Liftable[$T] {
+         def apply(value: $T): Tree = $reflect
+       }
+       $implicitName
+     """
   }
 }
 
--- macros/liftable/src/main/scala/com/github/ivellien/pgquery/liftable/LiftableEnumeration.scala
+++ macros/liftable/src/main/scala/com/github/ivellien/pgquery/liftable/LiftableEnumeration.scala
@@ -18,103 +18,26 @@ object LiftableEnumeration {
     // hack: adaptation of the original code - expects to be called from a macro which contains
     // a context `c` - which is kind of a convention at this point
 
-    val prefix = Select(
-      Select(c.prefix.tree, TermName("c")),
-      TermName("universe")
-    )
-
     val T = weakTypeOf[T]
     val symbolT = T.typeSymbol
 
     val E = T match { case TypeRef(outer, _, _) => outer }
     val symbolE = E.typeSymbol
 
-    /* Tree produced by the following quasiquote:
-     * q"reify(${c.mirror.staticModule(symbolE.fullName)}).tree"
-     */
-    val objectName = Select(
-      Apply(
-        Select(prefix, TermName("reify")),
-        List(Ident(c.mirror.staticModule(symbolE.fullName)))
-      ),
-      TermName("tree")
-    )
-    val value = "value"
-    val arguments = List(
-      Apply(
-        Select(prefix, TermName("Literal")),
-        List(
-          Apply(
-            Select(prefix, TermName("Constant")),
-            List(Select(Ident(TermName("value")), TermName("id")))
-          )
-        )
-      )
-    )
+    val objectName = q"reify(${c.mirror.staticModule(symbolE.fullName)}).tree"
 
-    /* Tree produced by the following quasiquote:
-     * q"Apply($objectName, List(..$arguments))"
-     */
-    val reflect = Apply(
-      Select(prefix, TermName("Apply")),
-      List(objectName, Apply(Ident(definitions.ListModule), arguments))
-    )
+    val arguments = q"List(Literal(Constant(value.id)))"
 
-    val implicitName = TermName(symbolT.name.encoded ++ "Liftable")
-    /* Tree produced by the following quasiquote:
-     * q"""
-     *   implicit object $implicitName extends Liftable[$T] {
-     *     def apply(value: $T): Tree = $reflect
-     *   }
-     *   $implicitName
-     * """
-     */
+    val reflect = q"Apply($objectName, $arguments)"
 
-    Block(
-      List(
-        ModuleDef(
-          Modifiers(IMPLICIT),
-          implicitName,
-          Template(
-            List(
-              AppliedTypeTree(
-                Select(prefix, TypeName("Liftable")),
-                List(TypeTree(T))
-              )
-            ),
-            noSelfType,
-            List(
-              DefDef(
-                Modifiers(),
-                nme.CONSTRUCTOR,
-                List(),
-                List(List()),
-                TypeTree(),
-                Block(List(pendingSuperCall), Literal(Constant(())))
-              ),
-              DefDef(
-                Modifiers(),
-                TermName("apply"),
-                List(),
-                List(
-                  List(
-                    ValDef(
-                      Modifiers(PARAM),
-                      TermName(value),
-                      TypeTree(T),
-                      EmptyTree
-                    )
-                  )
-                ),
-                Select(prefix, TypeName("Tree")),
-                reflect
-              )
-            )
-          )
-        )
-      ),
-      Ident(implicitName)
-    )
+    val implicitName = TermName(symbolT.name.encodedName.toString ++ "Liftable")
+
+    q"""
+       implicit object $implicitName extends Liftable[$T] {
+         def apply(value: $T): Tree = $reflect
+       }
+       $implicitName
+     """
   }
 }
 
