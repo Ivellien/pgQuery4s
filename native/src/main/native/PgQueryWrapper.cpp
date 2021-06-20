66df3646f1664b0742c5e6cbf2606413e888f3a2
Petr Hron <petr.hron@blindspot.ai>
2021-06-20 11:17:32 +0200

    Added freeing of PgQueryParseResult

--- native/src/main/native/PgQueryWrapper.cpp
+++ native/src/main/native/PgQueryWrapper.cpp
@@ -7,9 +7,14 @@ JNIEXPORT jstring JNICALL Java_com_github_ivellien_pgquery_native_PgQueryWrapper
   (JNIEnv *env, jobject obj, jstring string) {
     const char* str = env->GetStringUTFChars(string, 0);
     char *cap = (char *) malloc(strlen(str) + 1);
+    
     strcpy(cap, str);
     PgQueryParseResult result = pg_query_parse(cap);
-
     free(cap);
-    return env->NewStringUTF(result.parse_tree);
+
+    char *res = (char *) malloc(strlen(result.parse_tree) + 1);
+    strcpy(res, result.parse_tree);
+    pg_query_free_parse_result(result);
+
+    return env->NewStringUTF(res);
   }
